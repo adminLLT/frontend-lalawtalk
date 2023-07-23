9721471b73c5277670435e1b3ca00dd7d0becd85
Thanawatzeromos <talklalaw@gmail.com>
2023-07-23 19:24:51 +0700

    navbar

---
+++ Lalawtalk/src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+   
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container navbar-container"> 
+                  <a class="navbar-brand" href="#">
+                    <img src="https://via.placeholder.com/60x60" width="60" height="60" alt="Logo">
+                    LALAWTALK
+                  </a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">หน้าหลัก</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">เกี่ยวกับเรา</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">บริการ</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">ติดต่อเรา</a>
+                      </li>
+                    </ul>
+                    <div class="dropdown d-flex align-items-center order-1">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" id="profileDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="bg-primary rounded-circle" style="width: 40px; height: 40px;"></div>
+                      </a>
+                      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+                        <li><a class="dropdown-item" href="#">โปรไฟล์ของฉัน</a></li>
+                        <li><a class="dropdown-item" href="#">เอกสารของฉัน</a></li>
+                        <li><a class="dropdown-item" href="#">แชท</a></li>
+                        <li><a class="dropdown-item" href="#">การตั้งค่า</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">ออกจากระบบ</a></li>
+                      </ul>
+                    </div>
+                    <div class="bg-warning rounded" style="width: 100px; height: 40px;">
+                      <div class="d-flex align-items-center justify-content-center" style="height: 100%; color: black; font-size: 20px; font-weight: 700;">฿ 10.00</div>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+        </div>
+        
+      
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Lalawtalk/src/views/HomeLanding.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <component :is="currentTab" ></component>
+    <div>
+<h1>hello madafak</h1>
+    </div>
+</template>
+
+<script>
+import  NavBar from '../components/NavBar.vue'
+export default {
+    components: {
+    NavBar,
+   
+  },
+  data() {
+    return {
+      currentTab: 'NavBar',
+    
+    }
+    
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
